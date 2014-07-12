23aee6ce26cb3f10145ba418392eca76a75d0418
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-07-12 15:13:53 -0400

    Change in the factory generation to allow randomness

--- spec/factories/programs.rb
+++ spec/factories/programs.rb
@@ -2,27 +2,27 @@
 
 FactoryGirl.define do
   factory :program do
-    gender "M" # M, W, both
-    min_age 15
-    total_homeless_years 1 # 0-1
-    total_homeless_month 6 # 0-12 # months homeless, at least that
-    total_homeless_housed_again 1 # 0-10, how many, at least that
-    chronic "Can" # Can, Cannot, Must
-    money_regular_basis "Can" # Can, Cannot, Must
-    drug_alcohol_abuse "Must" # Can, Cannot, Must
-    drug_alcohol_treatment "Can" # Can, Cannot, Must
-    alcohol_daily_last_month "Cannot" # Can, Cannot, Must
-    hosp_emotional_nerves "Can" # Can, Cannot, Must
-    mh_professional_appnt "Can" # Can, Cannot, Must
-    mht_against_will "Must" # Can, Cannot, Must
-    trauma_not_saught_treatment "Cannot" # Can, Cannot, Must
-    dual_diagnosis "Can" # Can, Cannot, Must
-    mobility_limits "Cannot" # Can, Cannot, Must
-    citizenship_status "Must" # Can, Cannot, Must
-    military "Can" # Can, Cannot, Must
-    jail "Cannot" # Can, Cannot, Must
-    prison "Can" # Can, Cannot, Must
-    ci_incarceration "Cannot" # Can, Cannot, Must
-    hiv_aids "Must" # Can, Cannot, Must
+    gender %w(M W T B).sample # M, W, both
+    min_age { rand(0..80) }
+    total_homeless_years {rand(0..1)} # 0-1
+    total_homeless_month {rand(0..12)} # 0-12 # months homeless, at least that
+    total_homeless_housed_again {rand(0..10)} # 0-10, how many, at least that
+    chronic { %w(Can Cannot Must).sample }
+    money_regular_basis { %w(Can Cannot Must).sample }
+    drug_alcohol_abuse { %w(Can Cannot Must).sample }
+    drug_alcohol_treatment { %w(Can Cannot Must).sample }
+    alcohol_daily_last_month { %w(Can Cannot Must).sample }
+    hosp_emotional_nerves { %w(Can Cannot Must).sample }
+    mh_professional_appnt { %w(Can Cannot Must).sample }
+    mht_against_will { %w(Can Cannot Must).sample }
+    trauma_not_saught_treatment { %w(Can Cannot Must).sample }
+    dual_diagnosis { %w(Can Cannot Must).sample }
+    mobility_limits { %w(Can Cannot Must).sample }
+    citizenship_status { %w(Can Cannot Must).sample }
+    military { %w(Can Cannot Must).sample }
+    jail { %w(Can Cannot Must).sample }
+    prison { %w(Can Cannot Must).sample }
+    ci_incarceration { %w(Can Cannot Must).sample }
+    hiv_aids { %w(Can Cannot Must).sample }
   end
 end
