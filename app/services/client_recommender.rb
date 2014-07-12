3d99fd1c46a1e34ceffdf7c05ba548297e3e3587
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-07-12 15:39:20 -0400

    Added desc to both alg methods

--- app/services/client_recommender.rb
+++ app/services/client_recommender.rb
@@ -1,8 +1,17 @@
 class ClientRecommender
+  # 1. Looks at the apartment.programs
+  # 2. Selects all clients that are eligible for the program
+
+  # Returns array of clients that are eligible for funding in the apartment
   def get_qualifying_clients(apartment)
 
   end
 
+  # 1. Takes output of get_qualifying_clients as possible tenants
+  # 2. Takes criteria of apartment and apartment.housing_complex
+  # 3. Looks at each client and sees which criteria s/he is, isn't or maybe is eligible
+
+  # Returns a hash of clients and criteria on which they are eligible? Maybe
   def match_other_criteria(apartment)
 
   end
