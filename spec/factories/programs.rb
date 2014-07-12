e419fb59a55e18f67d0c995568d7a7437d00c779
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-07-12 13:45:47 -0400

    Added explanations and possible values in comments for factory

--- spec/factories/programs.rb
+++ spec/factories/programs.rb
@@ -2,28 +2,27 @@
 
 FactoryGirl.define do
   factory :program do
-    gender ""
-    min_age 1
-    total_homeless_years 1
-    total_homeless_years 1
-    total_homeless_month 1
-    total_homeless_housed_again 1
-    chronic "String"
-    money_regular_basis "MyString"
-    drug_alcohol_abuse "MyString"
-    drug_alcohol_treatment "MyString"
-    alcohol_daily_last_month "MyString"
-    hosp_emotional_nerves "MyString"
-    mh_professional_appnt "MyString"
-    mht_against_will "MyString"
-    trauma_not_saught_treatment "MyString"
-    dual_diagnosis "MyString"
-    mobility_limits "MyString"
-    citizenship_status "MyString"
-    military "MyString"
-    jail "MyString"
-    prison "MyString"
-    ci_incarceration "MyString"
-    hiv_aids "MyString"
+    gender "" # M, W, both
+    min_age 15
+    total_homeless_years 1 # 0-1
+    total_homeless_month 1 # 0-12 # months homeless, at least that
+    total_homeless_housed_again 1 # 0-10, how many, at least that
+    chronic "String" # Can, Cannot, Must
+    money_regular_basis "MyString" # Can, Cannot, Must
+    drug_alcohol_abuse "MyString" # Can, Cannot, Must
+    drug_alcohol_treatment "MyString" # Can, Cannot, Must
+    alcohol_daily_last_month "MyString" # Can, Cannot, Must
+    hosp_emotional_nerves "MyString" # Can, Cannot, Must
+    mh_professional_appnt "MyString" # Can, Cannot, Must
+    mht_against_will "MyString" # Can, Cannot, Must
+    trauma_not_saught_treatment "MyString" # Can, Cannot, Must
+    dual_diagnosis "MyString" # Can, Cannot, Must
+    mobility_limits "MyString" # Can, Cannot, Must
+    citizenship_status "MyString" # Can, Cannot, Must
+    military "MyString" # Can, Cannot, Must
+    jail "MyString" # Can, Cannot, Must
+    prison "MyString" # Can, Cannot, Must
+    ci_incarceration "MyString" # Can, Cannot, Must
+    hiv_aids "MyString" # Can, Cannot, Must
   end
 end
