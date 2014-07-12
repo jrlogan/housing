1a5ccba7a416f4d7c7b8449ce2fdf496613b4af8
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-07-12 15:43:20 -0400

    Fixed the factory generator

--- spec/factories/apartments.rb
+++ spec/factories/apartments.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     floor { rand 1..5 }
     max_capacity { rand 1..4 }
     pets_allowed { [true, false].sample }
-    wheelchair_accessible { %w(Yes, No, Maybe).sample } # Yes, No, Maybe
+    wheelchair_accessible { %w(Yes No Maybe).sample } # Yes, No, Maybe
     smoking { [true, false].sample }
     information "This is an apartment from heaven."
     housing_complex { FactoryGirl.create(:housing_complex) }
