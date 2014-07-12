8bdc990ec97154e3138d2866ea49e8cd0929edd2
Taylor Stib <tstib3@gmail.com>
2014-07-12 14:06:01 -0400

    Program controller and new program view

---
+++ app/controllers/program_controller.rb
@@ -0,0 +1,23 @@
+class ProgramController < ApplicationController
+  def new
+    @program = Program.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'program/new'
+
+  get 'program/create'
+
+  get 'program/update'
+
+  get 'program/edit'
+
+  get 'program/destroy'
+
+  get 'program/index'
+
+  get 'program/show'
+
   devise_for :users
 
   root to: 'home#index'
